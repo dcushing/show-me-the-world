d988f62a82960ced313ba24588701c2858766c14
Danielle Cushing <daniellecushing@Danielles-Stuff--Things.local>
2017-08-16 22:45:29 +0700

    finished pages logic

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,16 @@
 class PlacesController < ApplicationController
-  #before_action :set_place
   
   def index
-    # do something
-  end
-  
-  def show
-    # show the _trip partial when the "more info" buttom is clicked
-    respond_to do |format|
-      format.js #{render layout: false}
-    end
     @place = Place.order("random()").first
   end
   
+  def show
+  end
+  
+  def info
+  end
+  
   private
-  #def set_place
-  #  @place = Place.take
-  #end
   
   def place_params
     params.require(:place).permit(:city, :city_ascii, :lat, :lng, :pop, :county, :iso2, :iso3, :province)
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   
-  resources :places, only: [:index, :show]
+  resources :places, only: [:index, :show, :info]
   
   
   scope "/:locale" do
     resources :static_pages, only: [:show]
   end
   
-  root 'places#show'  
+  root 'places#index'  
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
-  get 'new_place' => 'places#show'
+  #get 'new_place' => 'places#show'
+  #get '/more_info' => 'places#info', as: "more_info"
+  get 'info' => 'places#info'
   
 end
